3a59e0853d9cd67a787facc6b970ddf8f0b3a54b
BenDavies1218 <148213274+BenDavies1218@users.noreply.github.com>
2024-05-16 21:10:51 +1000

    feat: update blog function

--- src/components/EntryParent.jsx
+++ src/components/EntryParent.jsx
@@ -2,17 +2,15 @@ import { useState } from "react";
 import Entry from "./Entry";
 import EntryForm from "./EntryForm";
 
-export default function EntryParent(props) {
-  let [editMode, setEditMode] = useState(false);
+export default function EntryParent(props){
 
-  return (
-    <section>
-      {editMode ? (
-        <EntryForm entryData={props.entryData} />
-      ) : (
-        <Entry entryData={props.entryData} />
-      )}
-      <button onClick={setEditMode(!editMode)}>Toggle Edit Mode</button>
-    </section>
-  );
+	let [editMode, setEditMode] = useState(false);
+
+	return(
+		<section>
+			{editMode ? <EntryForm entryData={props.entryData} addEntry={props.addEntry} /> : <Entry entryData={props.entryData} /> }
+
+			<button onClick={() => setEditMode(!editMode)}>Toggle Edit Mode</button>
+		</section>
+	)
 }
